d0a42e03925a8934f4e378fe345cda7366db0553
diegocb81 <diegocb81@gmail.com>
2021-02-25 16:26:17 -0300

    Finalizando o projeto

--- Main.java
+++ Main.java
@@ -7,9 +7,19 @@ public class Main {
 
         ArrayList<String> nomes = new ArrayList<>(Arrays.asList("Pelé", "Maradona", "Ronaldinho", "Messi", "Romário", "Garrincha", "Zidane"));
         ArrayList<Integer> IDs = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7));
+        boolean encontrado = false;
 
         for (int i=0; i < nomes.size(); i++){
-            System.out.println("Nome: " + nomes.get(i) + " ID: " + IDs.get(i));
+            if (nomes.get(i).equals(args[0])){
+                System.out.println("Nome: " + nomes.get(i) + " ID: " + IDs.get(i));
+                encontrado = true;
+                break;
+            }
+
+        }
+
+        if (!encontrado){
+            System.out.println("404 Nome não encontrado!");
         }
 
     }
